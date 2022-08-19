08fbb621b99f2ae84a894d032d8f2861c0843a60
Hugo Dowson <hugo.dowson@mvfglobal.com>
2022-08-19 14:34:44 +0100

    Update webhook validation

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
-const posts = require('./posts')
+const qs = require('qs')
+require('dotenv').config();
 
-app.get('/posts', (req, res) => {
-  res.json(posts)
-})
+app.get('/verify-webhook', (req, res) => {
+  const mode = req.query['hub.mode']
+  const challenge = req.query['hub.challenge']
+  const token = req.query['hub.verify_token']
 
-app.get('/', (req, res) => {
-  console.log(req.query)
-
-  res.send('API')
+  if(mode === 'subscribe' && token === process.env.MY_VERIFY_TOKEN) {
+    res.status(200).send(challenge)
+  } else {
+    res.sendStatus(400)
+  }
 })
 
 app.listen(process.env.PORT || 3001, () => console.log("server is running"))
